483debf80d90b03748219ddbe96082f02569b2f7
Amy Kirasack <amykirasack@gmail.com>
2018-10-28 19:11:29 +0100

    build: Set up queries and mutations for yoga server

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,3 +1,16 @@
-const Mutation = {};
+const Mutation = {
+  async createItem(parent, args, ctx, info) {
+    // TODO: Check if logged in
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
+    return item;
+  }
+};
 
 module.exports = Mutation;
